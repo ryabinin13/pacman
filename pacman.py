4f3383f68d95db6dd852d30050433bcaa2059595
ryabinin13 <yura.ryabinin2018@mail.ru>
2023-09-23 01:08:07 +0300

    добавил сохранение карты в файл и восстановление ее из файла + теперь пакман не может выйти за границы поля

--- pacman.py
+++ pacman.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import pickle
 import itertools
 import numpy as np
 import math
@@ -20,6 +21,8 @@ class Pacman:
             for wall in walls:
                 if self.y - self.size >= wall[1]  and self.y - self.size <= wall[1] + 60 and self.x >= wall[0] and self.x <= wall[0] + 30:
                     return
+            if self.y <= SIZE_ONE_CELL:
+                return
             # Если стены нет, то изменяем координату y
             if self.y > 0:
                 self.y -= SIZE_ONE_CELL
@@ -28,35 +31,59 @@ class Pacman:
             for wall in walls:
                 if self.y + self.size >= wall[1] - 30 and self.y + self.size <= wall[1] + 30 and self.x >= wall[0] and self.x <= wall[0] + 30:
                     return
+            if self.y >=SIZE-SIZE_ONE_CELL:
+                return
             # Если стены нет, то изменяем координату y
             if self.y < SIZE - self.size:
                 self.y += SIZE_ONE_CELL
         elif self.direction == 'right':
             # Проверяем, есть ли стена на пути движения вправо
-            for wall in walls_unique:
+            for wall in walls:
                 if self.x + self.size >= wall[0] - 30 and self.x + self.size <= wall[0] + 30 and self.y >= wall[1] and self.y <= wall[1] +30:
                     return
+            if self.x >=SIZE-SIZE_ONE_CELL:
+                return
             # Если стены нет, то изменяем координату x
             if self.x < SIZE - self.size:
                 self.x += SIZE_ONE_CELL
         elif self.direction == 'left':
             # Проверяем, есть ли стена на пути движения влево
-            for wall in walls_unique:
+            for wall in walls:
                 if self.x - self.size >= wall[0] and self.x - self.size <= wall[0] + 60 and self.y >= wall[1] and self.y <= wall[1] + 30:
                     return
+            if self.x <=SIZE_ONE_CELL:
+                return
             # Если стены нет, то изменяем координату x
             if self.x > 0:
                 self.x -= SIZE_ONE_CELL
 
-pacman = Pacman(315,315)
+def add_data_to_file(data, filename):
+    try:
+        with open(filename, 'a') as file:
+            for d in data:
+                file.write(str(d) + ' ')
+            file.write('\n')
+    except:
+        print("Ошибка")
 
-class Wall:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.size = 30
-    def draw(self):
-        pygame.draw.rect(sc, (200, 200, 200), (coord[0], coord[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
+
+def read_list_from_line(filename, line_number, separator=' '):
+    result = []
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            line = lines[line_number-1].strip()
+            list_data = line.split(separator)
+            formatted_list = [x for x in list_data]
+            for item in formatted_list:
+                item = item.replace('[', '').replace(']', '')
+                result.append(item)
+                res = [int(item.replace(',', '')) for item in result]
+                r = list(zip(res[::2], res[1::2]))
+            return r
+
+    except:
+        print("Ошибка")
 
 SIZE = 600
 FPS = 60
@@ -64,16 +91,14 @@ SIZE_ONE_CELL = 30
 COUNT_OF_WALL =5
 COUNT_OF_AWARDS = 10
 
+pacman = Pacman(315,315)
 sc = pygame.display.set_mode((SIZE, SIZE))
 pygame.display.set_caption('pacman')
 clock = pygame.time.Clock()
 
-for i in range(SIZE):
-    if i % SIZE_ONE_CELL == 0:
-        pygame.draw.line(sc, (255,0,255), (0,i),(SIZE,i), 1)
-        pygame.draw.line(sc, (255, 0, 255), (i,0), (i,SIZE), 1)
-
 walls = []
+#строим стены
+
 for j in range(0, COUNT_OF_WALL):
     random_XY = [random.randint(0, SIZE) for i in range(2)]
     random_XY = [random_XY[i] - random_XY[i] % SIZE_ONE_CELL for i in range(2)]
@@ -108,26 +133,36 @@ for j in range(0, COUNT_OF_WALL):
         pygame.draw.rect(sc, (200, 200, 200), (coord[0], coord[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
         walls.append(coord)
 
-walls_unique = list(set(tuple(w) for w in walls))
 
-for awards in range(COUNT_OF_AWARDS):
+# walls_ = read_list_from_line('maps.txt', 6)
+# walls = []
+# for w in walls_:
+#     walls.append(list(w))
+
+
+for w in walls:
+    pygame.draw.rect(sc, (200,200,200), (w[0], w[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
+
+#строим награды
+awards_ = []
+for a in range(COUNT_OF_AWARDS):
     cross = False
     random_XY = [random.randint(0, SIZE) for i in range(2)]
-    random_XY = [random_XY[i] - random_XY[i] % SIZE_ONE_CELL for i in range(2)]
+    random_XY = [int(random_XY[i] - random_XY[i] % SIZE_ONE_CELL) for i in range(2)]
     for wall in walls:
         if random_XY == wall:
             cross = True
             break
     if cross == False:
-        random_XY = [random_XY[i] + SIZE_ONE_CELL/2 for i in range(2)]
+        random_XY = [int(random_XY[i] + SIZE_ONE_CELL/2) for i in range(2)]
         pygame.draw.circle(sc, (50, 100, 150), (random_XY[0], random_XY[1]), SIZE_ONE_CELL / 4)
-
+        awards_.append((random_XY[0], random_XY[1]))
+#строим клетки
 for i in range(SIZE):
     if i % SIZE_ONE_CELL == 0:
         pygame.draw.line(sc, (255,0,255), (0,i),(SIZE,i), 1)
         pygame.draw.line(sc, (255, 0, 255), (i,0), (i,SIZE), 1)
 
-print(len(walls_unique))
 
 pacman.draw()
 
@@ -149,5 +184,8 @@ while True:
             elif event.key == pygame.K_DOWN:
                 pacman.direction = "down"
                 pacman.move()
+            elif event.key == pygame.K_s:
+                add_data_to_file(walls, 'maps.txt')
+
     pacman.draw()
     pygame.display.flip()
