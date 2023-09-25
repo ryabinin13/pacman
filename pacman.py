157ca77acaceac0dd3a6635609f71ced01a7a5bc
ryabinin13 <yura.ryabinin2018@mail.ru>
2023-09-25 17:30:16 +0300

    добавил 100 карт, Ванек исправил вывод наград

--- pacman.py
+++ pacman.py
@@ -1,4 +1,3 @@
-import math
 import time
 import pygame
 import random
@@ -58,15 +57,6 @@ class Pacman:
             if self.x > 0:
                 self.x -= SIZE_ONE_CELL
 
-def add_data_to_file(data, filename):
-    try:
-        with open(filename, 'a') as file:
-            for d in data:
-                file.write(str(d) + ' ')
-            file.write('\n')
-    except:
-        print("Ошибка")
-
 def read_list_from_line(filename, line_number, separator=' '):
     result = []
     try:
@@ -81,37 +71,8 @@ def read_list_from_line(filename, line_number, separator=' '):
                 res = [int(item.replace(',', '')) for item in result]
                 r = list(zip(res[::2], res[1::2]))
             return r
-
     except:
         print("Ошибка")
-
-def check_block(walls, random_XY):
-    for w1 in walls:
-        if (random_XY[0] - SIZE_ONE_CELL == w1[0]) and (random_XY[1] == w1[1]):
-            for w2 in walls:
-                if (random_XY[0] - SIZE_ONE_CELL == w2[0]) and (random_XY[1] - SIZE_ONE_CELL == w2[1]):
-                    for w3 in walls:
-                        if (random_XY[0] == w3[0]) and (random_XY[1] - SIZE_ONE_CELL == w3[1]):
-                            return True
-            for w4 in walls:
-                if (random_XY[0] - SIZE_ONE_CELL == w4[0]) and (random_XY[1] + SIZE_ONE_CELL == w4[1]):
-                    for w5 in walls:
-                        if (random_XY[0] == w5[0]) and (random_XY[1] + SIZE_ONE_CELL == w5[1]):
-                            return True
-    for w6 in walls:
-        if (random_XY[0] + SIZE_ONE_CELL == w6[0]) and (random_XY[1] == w6[1]):
-            for w7 in walls:
-                if (random_XY[0] + SIZE_ONE_CELL == w7[0]) and (random_XY[1] - SIZE_ONE_CELL == w7[1]):
-                    for w8 in walls:
-                        if (random_XY[0] == w8[0]) and (random_XY[1] - SIZE_ONE_CELL == w8[1]):
-                            return True
-            for w9 in walls:
-                if (random_XY[0] + SIZE_ONE_CELL == w9[0]) and (random_XY[1] + SIZE_ONE_CELL == w9[1]):
-                    for w10 in walls:
-                        if (random_XY[0] == w10[0]) and (random_XY[1] + SIZE_ONE_CELL == w10[1]):
-                            return True
-    return False
-
 
 FPS = 60
 COUNT_OF_CELL = 11
@@ -125,77 +86,33 @@ sc = pygame.display.set_mode((SIZE, SIZE))
 pygame.display.set_caption('pacman')
 clock = pygame.time.Clock()
 
-walls = []
 #строим стены
+random_number_of_maps = random.randint(1,100)
 
-for j in range(0, COUNT_OF_WALL):
-    random_XY = [random.randint(0, SIZE) for i in range(2)]
-    random_XY = [random_XY[i] - random_XY[i] % SIZE_ONE_CELL for i in range(2)]
-
-
-    if check_block(walls, random_XY) == False:
-        walls.append(random_XY)
-        pygame.draw.rect(sc, (200,200,200), (random_XY[0], random_XY[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
-        random_right, random_left, random_up, random_down = [random.randint(0,4) for _ in range(4)]
-
-
-    for r in range(1, random_right):
-        coord = [random_XY[0] + r * SIZE_ONE_CELL, random_XY[1]]
-        if coord[0] > SIZE-SIZE_ONE_CELL:
-            continue
-        elif check_block(walls, coord) == False:
-            pygame.draw.rect(sc, (200, 200, 200), (coord[0], coord[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
-            walls.append(coord)
-
-    for l in range(1, random_left):
-        coord = [random_XY[0] -l * SIZE_ONE_CELL, random_XY[1]]
-        if coord[0] < 0+SIZE_ONE_CELL:
-            continue
-        elif check_block(walls, coord) == False:
-            pygame.draw.rect(sc, (200, 200, 200), (coord[0], coord[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
-            walls.append(coord)
-    for u in range(1, random_up):
-        coord = [random_XY[0], random_XY[1] -u * SIZE_ONE_CELL]
-        if coord[1] < 0+SIZE_ONE_CELL:
-            continue
-        elif check_block(walls, coord) == False:
-            pygame.draw.rect(sc, (200, 200, 200), (coord[0], coord[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
-            walls.append(coord)
-    for d in range(1, random_down):
-        coord = [random_XY[0], random_XY[1] +d * SIZE_ONE_CELL]
-        if coord[1] > SIZE-SIZE_ONE_CELL:
-            continue
-        elif check_block(walls, coord) == False:
-            pygame.draw.rect(sc, (200, 200, 200), (coord[0], coord[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
-            walls.append(coord)
-
-
-
-#walls_ = read_list_from_line('maps.txt', 8)
-#walls = []
-#for w in walls_:
-#    walls.append(list(w))
-walls_unique = list(set(tuple(w) for w in walls))
-print(len(walls_unique))
+walls_ = read_list_from_line('maps.txt', random_number_of_maps)
+walls = []
+for w in walls_:
+   walls.append(list(w))
 
 for w in walls:
     pygame.draw.rect(sc, (200,200,200), (w[0], w[1], SIZE_ONE_CELL, SIZE_ONE_CELL))
 
 #строим награды
 awards = []
-for a in range(COUNT_OF_AWARDS):
-    score_font = pygame.font.SysFont("Arial", 24)
+score_font = pygame.font.SysFont("Arial", 24)
+a = 0
+while( a != COUNT_OF_AWARDS ):
     cross = False
     random_XY = [random.randint(0, SIZE) for i in range(2)]
     random_XY = [int(random_XY[i] - random_XY[i] % SIZE_ONE_CELL) for i in range(2)]
     for wall in walls:
         if random_XY == wall:
             cross = True
-            break
     if cross == False:
         random_XY = [int(random_XY[i] + SIZE_ONE_CELL/2) for i in range(2)]
         pygame.draw.circle(sc, (50, 100, 150), (random_XY[0], random_XY[1]), SIZE_ONE_CELL / 4)
         awards.append((random_XY[0], random_XY[1]))
+        a += 1
 #строим клетки
 for i in range(SIZE):
     if i % SIZE_ONE_CELL == 0:
@@ -231,8 +148,12 @@ while True:
             elif event.key == pygame.K_DOWN:
                 pacman.direction = "down"
                 pacman.move()
-            elif event.key == pygame.K_s:
-                add_data_to_file(walls, 'maps.txt')
+            elif score == COUNT_OF_AWARDS - 1:
+                sc.fill((0, 0, 0))
+                game_over_text = game_over_font.render("Вы выиграли!", True, (255, 255, 255))  # для отображения
+                sc.blit(game_over_text, (350, 350))
+                time.sleep(1)
+                exit()
     pygame.draw.rect(sc, (0, 0, 0), (2, 2, 90, 25))
     score_count()
     pacman.draw()
